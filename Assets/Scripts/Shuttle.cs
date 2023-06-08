976bf54d07d0629292692e6c868fee93ee072303
Toiletcleaner666 <jiphaas@icloud.com>
2023-06-08 12:38:13 +0200

    New brance + new arts

---
+++ Assets/Scripts/Shuttle.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shuttle: MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject shuttle;
+    public GameObject dock;
+
+    public void OnButtonPress()
+    {
+        dock.SetActive(true);
+        shuttle.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/titleScreen.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class titleScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void SceneSwitch(int SceneId)
+    {
+        SceneManager.LoadScene(SceneId);
+    }
+}
