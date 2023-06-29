738ca44901ec2637d9175b736258bda8017feae6
Seinaru <34807@ma-web.nl>
2023-06-29 10:54:43 +0200

    Sounds and Im trying

--- Assets/Scripts/Room/Shuttle.cs
+++ Assets/Scripts/Room/Shuttle.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Shuttle: MonoBehaviour
@@ -9,12 +11,33 @@ public class Shuttle: MonoBehaviour
     public GameObject shuttle;
     public GameObject dock;
 
+    public float timerDuration = 2f;
+    public float timer;
     public GameObject SceneChange;
 
     public void OnButtonPress()
     {
-        dock.SetActive(true);
-        shuttle.SetActive(false);
-        SceneChange.SetActive(true);
+        StartTimer();
+
+        if (timer == 2f)
+        {
+            SceneChange.SetActive(true);
+            timer -= Time.deltaTime;
+        }
+        if (timer == 1f)
+        {
+            dock.SetActive(true);
+            shuttle.SetActive(false);
+        }
+        if (timer <= 0f || timer == 0)
+        {
+            timer = 0f;
+            SceneChange.SetActive(false);
+        }
+    }
+
+        public void StartTimer()
+    {
+        timer = timerDuration;
     }
 }
